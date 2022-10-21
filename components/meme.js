a04214e3b88677942abacc9fa602f834521bcee3
yanness168 <94063697+yanness168@users.noreply.github.com>
2022-10-20 23:22:16 -0400

    changed the apps name

--- components/meme.js
+++ components/meme.js
@@ -1,7 +1,7 @@
 import * as React from'react';
 import { useEffect} from 'react';
 import { connect } from 'react-redux'; //{connect} connects a React component with Redux store
-import { SafeAreaView,View, Text, Button, Image, TextInput, StyleSheet, StatusBar, FlatList, TouchableOpacity } from 'react-native';
+import { ScrollView,View, Text, Button, Image, TextInput, StyleSheet, StatusBar, FlatList, TouchableOpacity } from 'react-native';
 import { fetchMemes } from '../redux/actions/fetchMeme'
 import { store_name, store_url, clear_name, clear_url} from '../redux/actions/index'
 
@@ -33,34 +33,35 @@ const GetMeme = ({u, memes, loading, error, fetchMemes, url, name, store_name, s
     }
 
     return (
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             <TouchableOpacity onPress={handleOnClick} style={styles.bnt}>
                 <Text style={styles.bText}>Get some memes!!!</Text>
             </TouchableOpacity>
-            <FlatList
+            <FlatList nestedScrollEnabled
                 style={styles.memeList}
                 data={url}
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item, index }) => 
                 (<View styles={styles.flatlist}>
                     <Text>{name[index]}</Text>
-                    <Image source={item} style={{ width:200, height:200, borderWidth:1, margin: 10, borderColor:'#FFB6C1'}}/>
+                    <Image styles={styles.image} source={item} style={{ width:100, height:100, borderWidth:1, margin: 10, resizemode: 'cover',borderColor:'#FFB6C1'}}/>
                 </View>)}/>
-        </SafeAreaView>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        margin: 25,
+        marginHorizontal: 30,
+        marginVertical: 47,
         backgroundColor: '#fff',
-        flex: 3,
-        flexDirection: 'row',
+        flex: 1,
+        flexDirection: 'column',
         flexWrap: 'wrap',
         writingDirection: 'rtl',
-        alignItems: 'flex-start',
-        justifyContent: 'space-around',
         backgroundColor: "lightblue",
+        alignItems: 'center',
+        justifyContent: 'space-around',
     },
     bnt:{
         height: 30,
@@ -73,12 +74,10 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: 'bold',
     },
-    memeList:{
-        marginHorizontal: 300,
-        width:"100%",
-    },
     flatlist: {
         margin: 10,
+        width: 300,
+        height: 300,
         backgroundColor: 'black',
         backgroundWidth: 2,
     },
